858871ad50c775677c19e32997b9258431156064
clarelimo <clare.limo@student.moringaschool.com>
2021-07-25 02:11:51 +0300

    get repo function added

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -3,12 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from './user';
+import { Repo } from './repo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
   user!:User;
+  repos!:Repo;
+
   constructor(private http:HttpClient) { }
 
   getProfile(username:string){
@@ -42,5 +45,31 @@ export class SearchService {
       return promise;
     }
 
+    getRepo(username:string){
+      interface ApiResponse{
+        name:string;
+        url:string;
+        description:string;
+        language:string;
+        
+      }
+      let promise = new Promise<void>((resolve,reject) =>{
+        this.http.get<ApiResponse>(`https://api.github.com/users/'${username}"/repos?order=created&sort=asc?access_token="${environment.apiKey}`).toPromise().then
+        (response => {
+            this.repos.name = response.name;
+            this.repos.url = response.url;
+            this.repos.description = response.description;
+            this.repos.language = response.language;
+  
+          resolve()
+        },
+        error=>{
+          this.repos.name = "We couldn’t find any repositories matching the name given"
+  
+          reject(error)
+          })
+        })
+        return promise;
 
+    }
   }
