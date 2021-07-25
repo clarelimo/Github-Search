df9eac020eb5235a513614d76dc60df06646aa02
clarelimo <clare.limo@student.moringaschool.com>
2021-07-25 19:42:20 +0300

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'user',component:UserComponent},
+  {path:'search',component:SearchComponent},
+  {path:'header',component:HeaderComponent},
+  {path: '', redirectTo:"/user", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
